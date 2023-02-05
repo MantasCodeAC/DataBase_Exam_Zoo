a53ab309ca0e5cd09225ee0690acb48209b81eeb
Mantas Razanta <mantas.razanta@codeacademylt.onmicrosoft.com>
2023-02-05 23:07:31 +0200

    Test Added

---
+++ DataBase_Exam_Zoo_WindowsFormTests/Form5_AnimalCreatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBase_Exam_Zoo_WindowsForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBase_Exam_Zoo.Models;
+using DataBase_Exam_Zoo.Repositories;
+
+namespace DataBase_Exam_Zoo_WindowsForm.Tests
+{
+    [TestClass()]
+    public class Form5_AnimalCreatorTests
+    {
+        [TestMethod()]
+        public void giveDepartmentIDTest()
+        {
+            using(var context = new ZooDbContext())
+            {
+                // Arrange
+                Department selectedDepartment = context.Departments.First(x => x.DepartmentId == 1);
+
+                // Act
+                Form5_AnimalCreator form = new Form5_AnimalCreator();
+                Department departmentToTest = form.giveDepartmentID("ID_1_ 'Department Of The Birds'", context);
+
+                // Assert
+                Assert.AreEqual(selectedDepartment.Name, departmentToTest.Name);
+            }
+        }
+    }
+}
